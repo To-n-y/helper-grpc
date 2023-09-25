2c125118511720b1476da65c5128932100c21932
Tony <realvitl@yandex.ru>
2023-09-25 14:55:10 +0300

    upd: migrations

--- alembic/versions/7574da15f1ec_initial.py
+++ alembic/versions/7574da15f1ec_initial.py
@@ -7,9 +7,9 @@ Create Date: 2023-09-23 20:54:34.836222
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '7574da15f1ec'
