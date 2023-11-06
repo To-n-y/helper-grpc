2ac9e6cc7274860fa81a82dbaa869a47249b67d5
Tony <realvitl@yandex.ru>
2023-11-06 21:11:46 +0300

    upd: tests

--- tests/events_test.py
+++ tests/events_test.py
@@ -8,7 +8,8 @@ from service.observer_service import ObserverService
 
 class TestObserver:
     @pytest.mark.asyncio
-    async def test_create_event(self, temp_db):
+    @pytest.mark.slow
+    async def test_create_read_event(self, temp_db):
         request_mock = Mock()
         request_mock.day = 1
         request_mock.name = 'testname'
@@ -34,3 +35,24 @@ class TestObserver:
         )
         context_mock.set_code.assert_called_with(StatusCode.INVALID_ARGUMENT)
         context_mock.set_details.assert_called_with("0 <= day <= 7")
+
+        request_mock.id = 1
+        events_list = await ObserverService().ListEvent(request=request_mock, context=context_mock)
+        assert len(events_list.events) == 1
+        assert events_list.events[0].id == 1
+        assert events_list.events[0].day == 1
+
+        event = await ObserverService().ReadEventById(request=request_mock, context=context_mock)
+        assert event.event.name == 'testname'
+
+        request_mock.name = 'newname'
+        event = await ObserverService().UpdateEventById(request=request_mock, context=context_mock)
+        assert event.event.name == 'newname'
+
+        await ObserverService().DeleteEventById(request=request_mock, context=context_mock)
+
+        events = await ObserverService().ListEvent(request=request_mock, context=context_mock)
+        assert len(events.events) == 0
+
+
+
